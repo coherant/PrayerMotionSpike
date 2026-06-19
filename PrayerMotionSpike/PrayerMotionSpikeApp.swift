0fab1dc42c4156eb675880cb5ebc7ece34524095
Oscar Huseyin <oscar@xienta.io>
2026-06-20 00:16:51 +1000

    Initial Commit

---
+++ PrayerMotionSpike/PrayerMotionSpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerMotionSpikeApp.swift
+//  PrayerMotionSpike
+//
+//  Created by Oscar Huseyin on 20/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct PrayerMotionSpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
